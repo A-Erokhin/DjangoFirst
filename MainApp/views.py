bfc8a97ce96852773f258db89e7442c04aad0be1
A-Erokhin <a-erkhin@yandex.ru>
2022-11-22 12:35:30 +0300

    part1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,54 +1,49 @@
 from django.shortcuts import render, HttpResponse
 from django.http import Http404
+from MainApp.models import Item
 
-author = {
-    "name": "Александр",
-    "surname": "Ерохин",
-    "email": "a-erokhin@yandex.ru",
-    "phone": "+7-999-999-99-99"
-}
 
-items = [
-    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-    {"id": 5, "name": "Coca-cola Санкционая 1 литр", "quantity": 12},
-    {"id": 7, "name": "Картофель фри", "quantity": 0},
-    {"id": 8, "name": "Кепка", "quantity": 124},
-]
+# items = [
+#     {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+#     {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+#     {"id": 5, "name": "Coca-cola Санкционая 1 литр", "quantity": 12},
+#     {"id": 7, "name": "Картофель фри", "quantity": 0},
+#     {"id": 8, "name": "Кепка", "quantity": 124},
+# ]
 
 # Create your views here.
 def home(request):
-    # text = f"""
-    # <h1>"Изучаем django"</h1>
-    # <b>Автор</b>: <i>Ерохин А.П.</i>
-    # """
-    # return HttpResponse(text)
     return render(request, 'index.html')
 
-def about(request):
-    text = f"""
-    Имя: <b>{author['name']}</b><br>
-    Фамилия: <b>{author['surname']}</b><br>
-    телефон: <b>{author['phone']}</b><br/> 
-    email: <b>{author['email']}</b><br>
-    """
-    return HttpResponse(text)
+def page_item(request, id):
+    item = Item.objects.get(id = id)
+    # for item in items:
+    #     if item['id'] == id:
+    context = {
+                "item": item
+            }
+    return render(request, 'item_page.html', context)
+    # raise Http404(f"Товар с id={id} не найден")
 
-def page_item(reqest, id):
-    for item in items:
-        if item['id'] == id:
-            text = f"""
-                Название: <b>{item['name']}</b><br>
-                Количество: <b>{item['quantity']}</b><br/> 
-                """
-            return HttpResponse(text)
-    raise Http404(f"Товар с id={id} не найден")
+def items_list(request):
+    items = Item.objects.all()
+    context = {
+        "items": items
+    }
+    return render(request, 'items_list.html', context)
 
-def items_list(reqest):
-    text = """
-    <ol>
-    """
-    for item in items:
-        text += f"<a href='/item/{item['id']}'><li>{item['name']}</li></a>"
-    text += "</ol>"
-    return HttpResponse(text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
